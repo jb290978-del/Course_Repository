4de67f62d23fa2ab7b660deb1961d98674206541
James <jbelt@gmx.com>
2026-08-30 18:57:04 +0100

    Matthes

---
+++ Matthes/Chapter2/favourite_number.py
@@ -0,0 +1,2 @@
+fave = 52
+print(f"My favourite number is {fave}.")    #tell everyone what my favourite number is
---
+++ Matthes/Chapter2/number_eight.py
@@ -0,0 +1,7 @@
+print(5+3)
+print(13-5)
+print(16/2)
+print(type(16/2))
+multiply = 2*4
+print(multiply)
+print(type(multiply))
