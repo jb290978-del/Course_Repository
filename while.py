7df7a2e75adac785eb69f6c039f00d2b0daa1743
James <jbelt@gmx.com>
2026-08-30 19:17:44 +0100

    while

--- while.py
+++ while.py
@@ -1,8 +1,8 @@
 #while factorial loop
-x = 1
-y = int(input("Root: "))+1
-output = 1
-while (x < y):
-    output = output * x
-    print(output)
-    x += 1
+x = 1   #set initial value of x
+y = int(input("Root: "))+1  #ask user for the value what they want to factorialise and add 1
+output = 1  #initialise output value
+while (x < y):  #create while loop as long as x is less than y
+    output = output * x #multiply latest value of output by current x value
+    print(output)   #print current output value
+    x += 1 #increase value of x by 1
