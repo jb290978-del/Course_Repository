bf2cf7b68d20e97d04643a2feab56cecb26cc3e5
James <jbelt@gmx.com>
2026-08-30 18:32:16 +0100

    Matthes

---
+++ Matthes/Chapter2/name_cases.py
@@ -0,0 +1,5 @@
+name = "james bElt"
+print(name)
+print(name.lower())
+print(name.upper())
+print(name.title())
---
+++ Matthes/Chapter2/personal_message.py
@@ -0,0 +1,5 @@
+name = input("What is your name?")
+if name == "Bailey":
+    print(f"Hello, {name}, would you like a cuddle today?")
+else:
+    print(f"Hello, {name}, would you like to learn some Python today?")
